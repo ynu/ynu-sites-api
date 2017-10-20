bc5c05b3a341bf06ac81c8ea939ffe619bf8aa9a
liudonghua123 <liudonghua123@gmail.com>
2017-10-20 18:20:47 +0800

    remove unused files

--- server/boot/datasources.production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  sites: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_DATABASE || 'sites',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-  }
-};
